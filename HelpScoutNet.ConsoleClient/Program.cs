4f9c0c041109b13101453901b66b54f6fc2ff09f
Alina Dev <a@alinadev.com>
2016-03-17 20:19:22 +0700

    Add basic implementation get client logic.

--- HelpScoutNet.ConsoleClient/Program.cs
+++ HelpScoutNet.ConsoleClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelpScoutNet.Request;
 
 namespace HelpScoutNet.ConsoleClient
 {
@@ -10,7 +11,15 @@ namespace HelpScoutNet.ConsoleClient
     {
         static void Main(string[] args)
         {
-            var client = new HelpScoutClient("api key");
+            var client = new HelpScoutClient("a9fe3bcbef4d989dc7d05f1b8b1f90359a23d61f");
+            var customersSearch =
+                client.SearchCustomers(new SearchRequest { Query = "(customer:\"johnappleseed@gmail.com\")" });
+            foreach (var searchresult in customersSearch.Items)
+            {
+                Console.WriteLine(searchresult.FirstName + searchresult.LastName);
+            }
+
+            Console.ReadKey();
         }
     }
 }
